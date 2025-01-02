610ac011a50bbc5175387229120fe28094b9a437
Imtiaz <imtiaz.4.us@gmail.com>
2025-01-01 20:28:46 -0500

    Fix PEP 8 E302 error by adding two blank lines before function

--- demo.py
+++ demo.py
@@ -1,4 +1,12 @@
+"""
+This script contains a simple function for demonstration purposes.
+"""
+
+
 def my_function():
+    """
+    A simple function that returns the value 5.
+    """
     a = 5
     return a
 
